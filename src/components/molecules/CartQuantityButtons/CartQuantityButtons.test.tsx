45539adb69f981d860f387ef65b724c1becf8b96
user1 <user1@gmail.com>
2025-03-23 12:53:41 +0000

    Add tests to CartQuantityButtons component

---
+++ src/components/molecules/CartQuantityButtons/CartQuantityButtons.test.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import CartQuantityButtons from "./CartQuantityButtons";
+import { mockProps, mockUpdateCartQuantity } from "./fixtures";
+
+describe("Component: CartQuantityButtons", () => {
+  it("SHOULD match snapshot", () => {
+    const component = render(<CartQuantityButtons {...mockProps} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it("SHOULD display number in cart correctly WHEN rendered", () => {
+    const { getByText } = render(<CartQuantityButtons {...mockProps} />);
+    expect(getByText(mockProps.numberInCart));
+  });
+
+  it("SHOULD call updateCartQuantity with correct values WHEN minus button is clicked", () => {
+    const { getByText } = render(<CartQuantityButtons {...mockProps} />);
+
+    fireEvent.click(getByText("-"));
+    expect(mockUpdateCartQuantity).toHaveBeenCalledWith(
+      mockProps.product,
+      mockProps.numberInCart - 1
+    );
+  });
+
+  it("SHOULD call updateCartQuantity with correct values WHEN plus button is clicked", () => {
+    const { getByText } = render(<CartQuantityButtons {...mockProps} />);
+
+    fireEvent.click(getByText("+"));
+    expect(mockUpdateCartQuantity).toHaveBeenCalledWith(
+      mockProps.product,
+      mockProps.numberInCart + 1
+    );
+  });
+});
