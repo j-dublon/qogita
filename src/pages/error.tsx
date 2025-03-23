6a619a1c6074fba93146e4b26cf185886533975d
user1 <user1@gmail.com>
2025-03-23 12:40:00 +0000

    Add default error page

---
+++ src/pages/error.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage: React.FC = () => {
+  return (
+    <div className="error-page">
+      <h1>Something went wrong!</h1>
+      <p>Sorry, an error has occurred. Please try again later.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
