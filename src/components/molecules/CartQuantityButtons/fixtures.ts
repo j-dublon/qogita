a6b1bb0b52f02ba01d641b69efecafca0e524a91
user1 <user1@gmail.com>
2025-03-23 17:40:11 +0000

    Update tests

--- src/components/molecules/CartQuantityButtons/fixtures.ts
+++ src/components/molecules/CartQuantityButtons/fixtures.ts
@@ -3,15 +3,6 @@ import { CartQuantityButtonsProps } from "@/types";
 export const mockUpdateCartQuantity = jest.fn();
 
 export const mockProps: CartQuantityButtonsProps = {
-  product: {
-    name: "Mock Product",
-    imageUrl: "http://your-product.com",
-    recommendedRetailPrice: 52,
-    recommendedRetailPriceCurrency: "GBP",
-    gtin: "987",
-    brandName: "Chanel",
-    categoryName: "Perfume",
-  },
   numberInCart: 3,
   updateCartQuantity: mockUpdateCartQuantity,
 };
--- src/components/organisms/ProductPage/mock-components.tsx
+++ src/components/organisms/ProductPage/mock-components.tsx
@@ -13,6 +13,6 @@ export const MockProductPage: FC<ProductPageProps> = ({
   <div>
     <div>Product Page</div>
     <div>{`Number in cart: ${numberInCart}`}</div>
-    <div onClick={() => updateCartQuantity(product, 4)}>Update Cart</div>
+    <div onClick={() => updateCartQuantity(4)}>Update Cart</div>
   </div>
 );
