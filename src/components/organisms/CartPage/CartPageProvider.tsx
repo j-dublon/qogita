6ac96c978cc6b13392abef54f0dbbab2b97a425d
user1 <user1@gmail.com>
2025-03-23 17:33:14 +0000

    Add loading state to Cart Page Provider

--- src/components/organisms/CartPage/CartPageProvider.tsx
+++ src/components/organisms/CartPage/CartPageProvider.tsx
@@ -1,23 +1,36 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import CartPage from "./CartPage";
 import { useCart } from "react-use-cart";
 import { Loading } from "../../atoms";
 import { ProductItem } from "@/types";
 
-const AllProductsPageProvider: FC = () => {
+const CartPageProvider: FC = () => {
   const { items, updateItemQuantity, cartTotal } = useCart();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (items && cartTotal) {
+      setLoading(false);
+    }
+  }, [items, cartTotal]);
 
   const handleUpdateCart = (item: ProductItem, newQuantity: number) => {
     updateItemQuantity(item.id, newQuantity);
   };
 
   return (
-    <CartPage
-      items={items}
-      handleUpdateCart={handleUpdateCart}
-      cartTotal={cartTotal}
-    />
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <CartPage
+          items={items}
+          handleUpdateCart={handleUpdateCart}
+          cartTotal={cartTotal}
+        />
+      )}
+    </>
   );
 };
 
-export default AllProductsPageProvider;
+export default CartPageProvider;
