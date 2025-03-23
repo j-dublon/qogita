ba9832f26699afca73805ef48b3180177932f74f
user1 <user1@gmail.com>
2025-03-23 14:44:19 +0000

    Add tests to ProductPageProvider

--- src/components/organisms/ProductPage/fixtures.ts
+++ src/components/organisms/ProductPage/fixtures.ts
@@ -1,4 +1,8 @@
-import { ProductPageProps } from "@/types";
+import { Product, ProductPageProps } from "@/types";
+import { Item } from "react-use-cart";
+
+export const mockAddItem = jest.fn();
+export const mockUpdateItemQuantity = jest.fn();
 
 export const mockProps: ProductPageProps = {
   product: {
@@ -12,4 +16,33 @@ export const mockProps: ProductPageProps = {
   },
   numberInCart: 3,
   updateCartQuantity: jest.fn(),
+};
+
+export const mockCartItem = {
+  ...mockProps.product,
+  id: mockProps.product.gtin,
+  price: mockProps.product.recommendedRetailPrice,
+  quantity: 3,
+};
+
+export const alternativeProduct: Product = {
+  name: "Other Mock Product",
+  imageUrl: "http://your-product-two.com",
+  recommendedRetailPrice: 35,
+  recommendedRetailPriceCurrency: "GBP",
+  gtin: "073",
+  brandName: "Chanel",
+  categoryName: "Perfume",
+};
+
+export const alternativeCartItem: Item = {
+  name: "Other Mock Product",
+  imageUrl: "http://your-product-two.com",
+  recommendedRetailPrice: 35,
+  price: 35,
+  recommendedRetailPriceCurrency: "GBP",
+  gtin: "073",
+  id: "073",
+  brandName: "Chanel",
+  categoryName: "Perfume",
 };
--- src/components/organisms/ProductPage/mock-components.tsx
+++ src/components/organisms/ProductPage/mock-components.tsx
@@ -1,5 +1,18 @@
+import { ProductPageProps } from "@/types";
 import React, { FC } from "react";
 
 export const MockUpdateCartQuantityButtons: FC = () => (
   <div>Update Cart Quantity Buttons</div>
+);
+
+export const MockProductPage: FC<ProductPageProps> = ({
+  product,
+  numberInCart,
+  updateCartQuantity,
+}) => (
+  <div>
+    <div>Product Page</div>
+    <div>{`Number in cart: ${numberInCart}`}</div>
+    <div onClick={() => updateCartQuantity(product, 4)}>Update Cart</div>
+  </div>
 );
