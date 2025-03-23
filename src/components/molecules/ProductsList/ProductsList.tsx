cab4e36e29c048122857772273825c0b82e35ac4
user1 <user1@gmail.com>
2025-03-23 09:08:57 +0000

    Update fixtures and snapshots

--- src/components/molecules/ProductsList/ProductsList.tsx
+++ src/components/molecules/ProductsList/ProductsList.tsx
@@ -2,15 +2,12 @@ import React, { FC } from "react";
 import { ProductsListProps } from "@/types";
 import ProductCard from "../ProductCard/ProductCard";
 
-const ProductsList: FC<ProductsListProps> = ({ products }) => {
-  console.log(products);
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
-      {products.map((product) => (
-        <ProductCard {...product} key={product.gtin} />
-      ))}
-    </div>
-  );
-};
+const ProductsList: FC<ProductsListProps> = ({ products }) => (
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
+    {products.map((product) => (
+      <ProductCard {...product} key={product.gtin} />
+    ))}
+  </div>
+);
 
 export default ProductsList;
