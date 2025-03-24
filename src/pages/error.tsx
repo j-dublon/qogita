ab4d6fee198eeaf7bf547c9de7e2138dfce152ee
user1 <user1@gmail.com>
2025-03-24 16:17:35 +0000

    Add link to error page for return to home

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const ErrorPage: React.FC = () => {
+  const router = useRouter();
+
   return (
     <div className="error-page">
       <h1>Something went wrong!</h1>
-      <p>Sorry, an error has occurred. Please try again later.</p>
+      <p>
+        Sorry, an error has occurred. Please{" "}
+        <button
+          onClick={() => router.push("/")}
+          className="text-blue-500 hover:underline"
+        >
+          go home
+        </button>{" "}
+        and try again.
+      </p>
     </div>
   );
 };
